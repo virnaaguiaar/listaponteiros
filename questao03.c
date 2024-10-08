ff94962ade033b069de5b5a7e03bd7b54427a8bd
Virna Aguiar <85238057+virnaaguiaar@users.noreply.github.com>
2024-10-08 20:42:53 -0300

    Create questao03.c

---
+++ questao03.c
@@ -0,0 +1,9 @@
+
+*q = &j;
+    <>'*q' é um inteiro, '&j' é um endereço, não são compatíveis
+
+i = (*&)j;
+    <> Não tem sentido desrefenciar um endereço sem incluir o endereço no cálculo ?!?!?!
+
+q = *p;
+    <> (?) '*' é int; 'q' é int*
