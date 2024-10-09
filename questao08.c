3d04c4d03fd9cb70233ab110873f545b0825d061
Virna Aguiar <85238057+virnaaguiaar@users.noreply.github.com>
2024-10-08 21:49:05 -0300

    Create questao08.c

---
+++ questao08.c
@@ -0,0 +1,11 @@
+◦ *(pulo + 2);
+      // Conteúdo do terceiro elemento do vetor 'pulo'
+
+◦ *(pulo + 4);
+      // Conteúdo do quinto elemento do vetor 'pulo'
+
+◦ pulo + 4;
+      // Endereço do quinto elemento do vetor 'pulo'
+
+◦ pulo + 2;
+      // Endereço do terceiro elemento do vetor 'pulo'
