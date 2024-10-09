2f4b5db259a44f10707b34c6d9eab316c5bcf98e
Virna Aguiar <85238057+virnaaguiaar@users.noreply.github.com>
2024-10-08 21:49:52 -0300

    Create questao09.c

---
+++ questao09.c
@@ -0,0 +1,8 @@
+p = mat + 1;
+        // novo endereço que aponta para o próximo lementor do vetor 'mat'
+
+p = mat;
+        // endereço que aponta para o 1° elemento do vetor 'mat'
+
+x = (*mat);
+        // valor do 1° elemento do vetor 'mat'
