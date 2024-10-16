d9042cc9a8f6563f1386dcdbd79c7705d27c93d1
Virna Aguiar <85238057+virnaaguiaar@users.noreply.github.com>
2024-10-15 22:18:10 -0300

    Update questao23.c

--- questao23.c
+++ questao23.c
@@ -1,14 +1,3 @@
-//Escreva uma função em linguagem C que escreva em um vetor a soma dos elementos
-//correspondentes de outros dois vetores. Os tamanhos dos vetores devem ser fornecidos pelo
-//usuário, portanto processos de alocação dinâmica de memória são necessários. Por exemplo, se
-//o primeiro vetor contiver os elementos 1, 3, 0 e -2, e o segundo vetor contiver os elementos 3, 5,
-//-3 e 1, o vetor de soma terá valores resultantes iguais a 4, 8, -3 e -1. A função deve receber 4
-//argumentos: os nomes dos três vetores e o número de elementos presentes em cada vetor.
-//Exemplo:
-//int n=4;
-// ...
-//soma_vetores(vet1, vet2, resultado, n);
-
 #include <stdio.h>
 #include <stdlib.h>
 
