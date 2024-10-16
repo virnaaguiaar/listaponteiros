98f0ec8dff6b2a9b15127bbfe1e0c955ed53342d
Virna Aguiar <85238057+virnaaguiaar@users.noreply.github.com>
2024-10-15 22:18:36 -0300

    Update questao24.c

--- questao24.c
+++ questao24.c
@@ -1,13 +1,3 @@
-//Crie uma função capaz de realizar multiplicação matricial da forma C = AxB. A função deve
-//////receber 6 argumentos: os ponteiros para as matrizes A, B e C, o número de linhas e colunas de A
-//e o número de colunas de B (assuma que o número de coluna de A é igual ao número de linhas
-//de B). O resultado da multiplicação deve ficar armazenado em C. Crie um programa para testar
-//sua implementação, capaz de utilizar a função de multiplicação e imprimir as três matrizes. A
-//função criada para multiplicação não deve realizar nenhum tipo de saída de dados no terminal.
-//Exemplo: para multiplicar duas matrizes (A e B) de dimensões 2x3 e 3x4, respectivamente (o
-//resultado deve ficar armazenado em C).
-//multiplica_matrizes(A, B, C, 2, 3, 4);
-
 #include <stdio.h>
 #include <stdlib.h>
 
