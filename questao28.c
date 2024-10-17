068b385e1df2bbd2e142255109dd220071a2c37d
Virna Aguiar <85238057+virnaaguiaar@users.noreply.github.com>
2024-10-17 17:44:20 -0300

    Atualizar o questao28.c

--- questao28.c
+++ questao28.c
@@ -1,4 +1,15 @@
-#include <stdio.h>
+Terceiro printf("%s ", *c[-2] + 3);
+
+c[-2]: Acessa o elemento que está dois elementos antes de c, ou seja, b[0] ("JUNIOR").
+*c[-2] retorna o ponteiro a[3] ("JUNIOR").
+*c[-2] + 3: Avança 3 posições na string "JUNIOR", resultando em "IOR".
+Portanto, imprime: IOR.
+Quarto printf("%s ", c[-1][-1] + 1);:
+
+c[-1]: Acessa o elemento que está um elemento antes de c, ou seja, b[2] ("MEDEIROS").
+c[-1][-1] é o mesmo que *c[-1], que resulta em a[1] ("MEDEIROS").
+c[-1][-1] + 1: Avança 1 posição na string "MEDEIROS", resultando em "EDEIROS".
+Portanto, imprime: EDEIROS.*/include <stdio.h>
 char *a[] = {"AGOSTINHO", "MEDEIROS", "BRITO", "JUNIOR"};
 char **b[] = {a + 3, a + 2, a + 1, a};
 char ***c = b;
@@ -14,4 +25,5 @@ int main() {
   printf("%s ", c[-1][-1] + 1); 
   return 0;
 }
+
 
